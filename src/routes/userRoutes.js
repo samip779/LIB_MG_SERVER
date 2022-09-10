5483b5bca47983e6ef348e33131250edbf1e5263
samip779 <samipluitel28@gmail.com>
2022-09-10 07:37:08 +0545

    implemented protect middleware in userProfile route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,12 +3,15 @@ import {
   register,
   listUsers,
   authUser,
+  getUserProfile,
 } from "../controllers/userController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", listUsers);
 router.post("/login", authUser);
 router.post("/register", register);
+router.get("/profile", protect, getUserProfile);
 
 export default router;
